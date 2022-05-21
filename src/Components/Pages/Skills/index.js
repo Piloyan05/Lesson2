41a8165b38e7a0f470310e33d2adee8198b7d407
Babken39 <babken1992@gmail.com>
2022-05-21 21:24:17 +0400

    skills

--- src/Components/Pages/Skills/index.js
+++ src/Components/Pages/Skills/index.js
@@ -1,9 +1,23 @@
 
 
-import React from 'react'
+import React from 'react';
+import style from "./Skills.module.css";
+import data from "./json.json";
 
-export default function Blog() {
+export default function Skills() {
+  
   return (
-    <div>Skills</div>
+    <section className="container">
+    <div className={style.skills_general}>
+      {data.map(i=>(
+        <div key = {i.id} className = {i.id===1 ? `${style.speak_blog}` :i.id===4 ? `${style.listening_blog}` :i.id===2 ? `${style.writing_blog}`:""}>
+          <img src={i.src}/>
+          <h2 >{i.name}</h2>
+          <p>{i.text}</p>
+          <button >Learn More</button>
+        </div>
+      ))}
+    </div>
+    </section>
   )
 }
